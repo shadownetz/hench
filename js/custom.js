66595815a10510d29ca6d8a83db24bd7aa9486f8
Jude <shadownetz@localhost.localdomain>
2019-06-05 02:12:02 +0100

    initial commit

--- js/custom.js
+++ js/custom.js
@@ -40,7 +40,7 @@ $(function () {
 // Control home navigation fixed scrolling mechanism
 window.onscroll = function () { navfixedScroll() };
 // Get the navbar
-var navbar = document.getElementById("breadcrumb");
+var navbar = document.getElementById("nav-bar");
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
